fd1e4851c9d9c4d2501c9a61e0418a2dddf0dcf9
Sean DeBellis <debellis@pdx.edu>
2014-12-24 10:14:35 -0800

    add message label and users label and box

--- ircClientGui.py
+++ ircClientGui.py
@@ -36,9 +36,15 @@ class interface:
         self.display_pad_size_y = self.DISPLAY_HEIGHT
         self.display_pad_size_x = self.DISPLAY_WIDTH
         
-        #used to determine the size of the display box for the room display
-        self.display_size_y = self.main_win_size_y - 4
-        self.display_size_x = self.main_win_size_x - int(self.main_win_size_x * .25)
+        #used to determine the size of the display box for the message display
+        self.msg_display_size_y = self.main_win_size_y - 4
+        self.msg_display_size_x = self.main_win_size_x - int(self.main_win_size_x * .25)
+        
+        #used to determine the size of the display box for the user display
+        self.users_display_min_y = 4
+        self.users_display_min_x = self.msg_display_size_x + 2
+        self.users_display_size_y = self.main_win_size_y - 4
+        self.users_display_size_x = self.main_win_size_x - self.msg_display_size_x - 2
 
         self.prompt = 'command-> '
         self.cursor_limit_left = len(self.prompt) + 1
@@ -53,6 +59,25 @@ class interface:
         self.create_room(self.current_room)
         self.draw_screen()
 
+    #for debugging purposes, draws all dimensional values into a special purpose room for evaluation
+    def dump_val(self):
+        room = 'dump room'
+        self.create_room(room)
+        self.post_to_room(room, 'self.main_win_size_y = ' + str(self.main_win_size_y))
+        self.post_to_room(room, 'self.main_win_size_x = ' + str(self.main_win_size_x))
+        self.post_to_room(room, 'self.input_pad_size_y = ' + str(self.input_pad_size_y))
+        self.post_to_room(room, 'self.input_pad_size_x = ' + str(self.input_pad_size_x))
+        self.post_to_room(room, 'self.display_pad_size_y = ' + str(self.display_pad_size_y))
+        self.post_to_room(room, 'self.display_pad_size_x = ' + str(self.display_pad_size_x))
+        self.post_to_room(room, 'self.msg_display_size_y = ' + str(self.msg_display_size_y))
+        self.post_to_room(room, 'self.msg_display_size_x = ' + str(self.msg_display_size_x))
+        self.post_to_room(room, 'self.users_display_min_y = ' + str(self.users_display_min_y))
+        self.post_to_room(room, 'self.users_display_min_x = ' + str(self.users_display_min_x))
+        self.post_to_room(room, 'self.users_display_size_y = ' + str(self.users_display_size_y))
+        self.post_to_room(room, 'self.users_display_size_x = ' + str(self.users_display_size_x))
+        self.switch_room(room)
+        self.draw_screen()
+    
     #function to be used in thread to read input from the keyboard and 
     #   then send input line to input buffer for displaying to the screen.
     #Issues:
@@ -62,6 +87,7 @@ class interface:
     #   processing and then they would pass into the post_to_room function
     #   -Need to deal with non-letter/number/punctuation input
     def keyboard_input(self):
+        
         string = ''
         while True:
             try:
@@ -81,12 +107,22 @@ class interface:
         self.main_win.clear()
         self.main_win.box()
         if self.current_room:
-            self.main_win.addstr(2, (self.main_win_size_x/2) - (len(self.current_room)/2), self.current_room)
+            self.main_win.addstr(1, (self.main_win_size_x/2) - (len(self.current_room)/2), self.current_room)
             self.refresh_current_room()
         else:
             self.main_win.addstr(2, (self.main_win_size_x/2) - (len('lobby')/2), 'Lobby')
-        self.main_win.hline(3,1, curses.ACS_BSBS, self.main_win_size_x -2)
-        self.main_win.vline(4,self.display_size_x + 1, curses.ACS_SBSB, self.main_win_size_y - 7)
+        
+        #line seperating room name
+        self.main_win.hline(2,1, curses.ACS_BSBS, self.main_win_size_x -2)                      
+
+        #displaying message label and line seperating label from msg box
+        self.main_win.addstr(3, self.msg_display_size_x / 2 - len('messages')/2, 'Messages')    
+        #self.main_win.hline(4, 1, curses.ACS_BSBS, self.msg_display_size_x)
+
+        #displaying users label and line seperating label from users box
+        self.main_win.addstr(3, self.users_display_min_x + (self.users_display_size_x /2 - len('users')/2), 'Users')
+        self.main_win.hline(4, 1, curses.ACS_BSBS, self.main_win_size_x - 2)
+        self.main_win.vline(3,self.msg_display_size_x + 1, curses.ACS_SBSB, self.main_win_size_y - 6)
         self.main_win.hline(self.main_win_size_y - 3, 1, curses.ACS_BSBS, self.main_win_size_x - 2)
         self.main_win.addstr(self.main_win_size_y - 2, 1, self.prompt)
         self.main_win.move(self.cursor_pos_y, self.cursor_limit_left)
@@ -109,7 +145,7 @@ class interface:
         rooms = self.rooms.keys()
         if room_name not in rooms:
             room_pad = curses.newpad(self.display_pad_size_y, self.display_pad_size_x)
-            num_msgs = 3
+            num_msgs = 4
             self.rooms[room_name] = (room_pad, num_msgs)
             self.current_room = room_name
             self.draw_screen()
@@ -156,11 +192,12 @@ if __name__ == '__main__':
 #Having trouble with switch, not sure where the problems are
 #   I need to to create test functions that to determine what being stored in the
 #   rooms and current_room and room_stack variable.
-    
+        
         gui = interface()
         gui.create_room('beef room')
         gui.create_room('dude room')
         current_room = 'dude room'
+        #gui.dump_val()                     #uncomment for debugging values
         time.sleep(.02)
         while True:
             string = gui.get_user_input()
